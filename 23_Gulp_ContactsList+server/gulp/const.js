4c65480c510dd61dc76a8b9e7e757d3168b0ea82
Olena Bardzik <ol.bardzik@gmail.com>
2023-04-26 00:12:04 +0300

    add: hometask 23 - gulp

---
+++ 23_Gulp_ContactsList+server/gulp/const.js
@@ -0,0 +1,12 @@
+module.exports.path = {
+    dest: "./dist",
+    destJs: "./app.js",
+    destCss: "./app.css",
+    htmlSrc: "./src/index.html",
+    cssSrc: "./src/**/*.css",
+    jsSrc: "./src/**/*.js",
+    allJsSrc: [
+        "./src/contactsAPI.js",
+        "./src/index.js"
+    ]
+}
---
+++ 23_Gulp_ContactsList+server/src/contactsAPI.js
@@ -0,0 +1,48 @@
+class ContactsApi {
+    static API = "https://62054479161670001741b708.mockapi.io/api/contacts"
+
+    static request (url = "", method = "GET", body) {
+        return fetch(`${ContactsApi.API}/${url}`, {
+            method,
+            body: body ? JSON.stringify(body) : undefined,
+            headers: {
+                "Content-type": "application/json",
+            }
+        })
+        .then((responce) => {
+            if (responce.ok) {
+                return responce.json();
+            }
+
+            throw new Error ("Cann't execute server request");
+        })
+    }
+
+    static getContactList() {
+        return ContactsApi.request()
+            .catch(() => {
+                throw new Error ("Cann't get contact list from server");
+            })
+    }
+
+    static create (contactData) {
+        return ContactsApi.request("", "POST", contactData)
+            .catch(() => {
+                throw new Error ("Cann't creat contact on server");
+            })   
+    }
+
+    static update (contactid, changes) {
+        return ContactsApi.request(contactid, "PUT", changes)
+            .catch(() => {
+                throw new Error ("Cann't update item");
+            })    
+    }
+
+    static delete (contactid) {
+        return ContactsApi.request(contactid, "DELETE")
+            .catch(() => {
+                throw new Error ("Cann't delete contact");
+            })     
+    }
+}
