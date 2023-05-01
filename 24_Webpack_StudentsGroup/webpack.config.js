357d0647e27f4f37f25c5e19e518e99b57f71676
Olena Bardzik <ol.bardzik@gmail.com>
2023-05-01 17:03:48 +0300

    24_webpack_students_group

---
+++ 24_Webpack_StudentsGroup/webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const mode = process.env.NODE_ENV || 'production';
+
+module.exports = {
+  mode,
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src', 'index.html')
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+  },
+};
